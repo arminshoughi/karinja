7ca6d094c0459c12aecbbbaef2d42f32ed1adfa8
armin shoghi <a.shoghi@cpol.co>
2023-03-29 13:59:08 +0330

    added STATICFILES_FINDERS into settings

--- backend/karinja/karinja/settings/base.py
+++ backend/karinja/karinja/settings/base.py
@@ -113,7 +113,10 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/2.2/howto/static-files/
 
 STATIC_URL = '/static/'
-
+STATICFILES_FINDERS = (
+    "django.contrib.staticfiles.finders.FileSystemFinder",
+    "django.contrib.staticfiles.finders.AppDirectoriesFinder",
+)
 DEFAULT_AUTO_FIELD = 'django.db.models.AutoField'
 REST_FRAMEWORK = {
     # Use Django's standard `django.contrib.auth` permissions,
