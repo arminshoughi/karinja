6dd0a4196f95cbc431fab0bba10fcdf627f86ddb
armin shoghi <a.shoghi@cpol.co>
2023-03-29 13:58:11 +0330

    update ProfileViewSet serializer for handling user types

--- backend/karinja/apps/share/views/user.py
+++ backend/karinja/apps/share/views/user.py
@@ -5,7 +5,8 @@ from rest_framework.permissions import AllowAny, IsAuthenticated
 from rest_framework.response import Response
 from rest_framework_simplejwt.tokens import RefreshToken
 
-from apps.share.serializers import UserBaseSerializer, UserRegisterSerializer
+from apps.share.consts import UserTypeChoices
+from apps.share.serializers import UserRegisterSerializer, EmployeeSerializer, CompanySerializer, UserBaseSerializer
 
 User = get_user_model()
 
@@ -17,6 +18,18 @@ class ProfileViewSet(RetrieveAPIView, UpdateAPIView, DestroyAPIView):
 
     def get_object(self):
         return self.request.user
+
+    def get_serializer(self, *args, **kwargs):
+        user = self.request.user
+        if not user.is_authenticated:
+            serializer_class = UserRegisterSerializer
+        elif user.typ == UserTypeChoices.EMPLOYEE:
+            serializer_class = EmployeeSerializer
+        else:
+            serializer_class = CompanySerializer
+
+        kwargs.setdefault('context', self.get_serializer_context())
+        return serializer_class(*args, **kwargs)
 
 
 class UserRegisterViewSet(CreateAPIView):
