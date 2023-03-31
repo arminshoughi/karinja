7e8e92863de900e6dc1dc2fe02d14d1f9583b4b6
armin shoghi <a.shoghi@cpol.co>
2023-03-31 16:12:33 +0330

    bug-fix

--- backend/karinja/utils/models.py
+++ backend/karinja/utils/models.py
@@ -3,7 +3,7 @@ from django.db import models
 
 class BaseModel(models.Model):
     created_at = models.DateTimeField(auto_now_add=True)
-    updated_at = models.DateTimeField(auto_now=True)
+    updated_at = models.DateTimeField(auto_now=True, blank=True, null=True)
     is_deleted = models.BooleanField(default=False)
     deleted_at = models.DateTimeField(blank=True, null=True)
 
