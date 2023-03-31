96483fe1911b59eb2de6913a76814e87a941f7cc
armin shoghi <a.shoghi@cpol.co>
2023-03-31 21:04:11 +0330

    added default pagination and default filter backend class

--- backend/karinja/karinja/settings/base.py
+++ backend/karinja/karinja/settings/base.py
@@ -128,6 +128,9 @@ REST_FRAMEWORK = {
     'DEFAULT_AUTHENTICATION_CLASSES': [
         'rest_framework_simplejwt.authentication.JWTAuthentication',
     ],
+    'DEFAULT_PAGINATION_CLASS': 'rest_framework.pagination.LimitOffsetPagination',
+    'PAGE_SIZE': 20,
+    'DEFAULT_FILTER_BACKENDS': ['django_filters.rest_framework.DjangoFilterBackend']
 }
 SIMPLE_JWT = {
     'ACCESS_TOKEN_LIFETIME': timedelta(days=1),
