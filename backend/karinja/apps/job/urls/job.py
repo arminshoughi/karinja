47c2df3e7c704b8b006f89b011ddb067373bdbe5
armin shoghi <a.shoghi@cpol.co>
2023-03-31 21:05:25 +0330

    added company job crud and apis (employee cruc not completed)

--- backend/karinja/apps/job/urls/job.py
+++ backend/karinja/apps/job/urls/job.py
@@ -1,10 +1,12 @@
 from django.urls import path, include
 from rest_framework.routers import DefaultRouter
 
-from apps.job.views import JobModelViewSet
+from apps.job.views import JobListViewSet, EmployeeJobModelViewSet, CompanyJobModelViewSet
 
 router = DefaultRouter()
-router.register('', JobModelViewSet, basename='job')
+router.register('company', CompanyJobModelViewSet, basename='company_jobs')
+router.register('employee', EmployeeJobModelViewSet, basename='employee_jobs')
+router.register('', JobListViewSet, basename='jobs')
 
 urlpatterns = [
     path(r'', include(router.urls)),
--- backend/karinja/apps/job/views/job.py
+++ backend/karinja/apps/job/views/job.py
@@ -1,18 +1,47 @@
 from django_filters.rest_framework import DjangoFilterBackend
-from rest_framework import filters
-from rest_framework.viewsets import ModelViewSet
 
-from apps.job.permissions import IsCompany
-from apps.job.serializers import JobModelBaseSerializer
+from rest_framework import mixins
+from rest_framework import filters
+from rest_framework.permissions import IsAuthenticated
+from rest_framework.viewsets import ModelViewSet, GenericViewSet
+
 from apps.job.services import JobService
 from utils.viewsets import UserRelatedDataRestricted
+from apps.job.permissions import IsCompany, IsEmployee
+from apps.job.serializers import JobModelBaseSerializer
 
 
-class JobModelViewSet(ModelViewSet, UserRelatedDataRestricted):
+class JobBaseModelMixin(object):
     queryset = JobService.all()
     serializer_class = JobModelBaseSerializer
-    permission_classes = [IsCompany]
+    permission_classes = [IsAuthenticated]
     filter_backends = [filters.SearchFilter, DjangoFilterBackend]
     filterset_fields = ['typ', 'sex', 'city', 'city__state', 'military_status']
     search_fields = ['title', 'skills']
 
+
+class JobListViewSet(JobBaseModelMixin, GenericViewSet, mixins.ListModelMixin, mixins.RetrieveModelMixin):
+    pass
+
+
+class CompanyJobModelViewSet(JobBaseModelMixin, ModelViewSet):
+    permission_classes = [IsCompany]
+
+    def get_queryset(self):
+        return self.queryset.filter(company=self.request.user)
+
+    def perform_create(self, serializer):
+        serializer.validated_data['company_id'] = self.request.user.id
+        serializer.save()
+
+    def perform_update(self, serializer):
+        serializer.validated_data['company_id'] = self.request.user.id
+        serializer.save()
+
+
+class EmployeeJobModelViewSet(
+    ModelViewSet,
+    UserRelatedDataRestricted,
+    JobBaseModelMixin
+):
+    permission_classes = [IsEmployee]
