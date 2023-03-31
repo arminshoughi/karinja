2ae5b94618309f87798b08235d33d46288a19fcf
armin shoghi <a.shoghi@cpol.co>
2023-03-31 17:33:54 +0330

    make settings ready for development

--- backend/karinja/karinja/settings/development.py
+++ backend/karinja/karinja/settings/development.py
@@ -3,9 +3,13 @@ from .base import *
 ALLOWED_HOSTS = ['*']
 
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, '../db.sqlite3'),
+    "default": {
+        "ENGINE": "django.db.backends.postgresql",
+        "NAME": 'karinja_db',
+        "USER": 'karinja_user',
+        "PASSWORD": 'karinja_password@',
+        "HOST": 'localhost',
+        "PORT": '5432',
     }
 }
 
