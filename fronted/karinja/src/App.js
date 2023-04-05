377eb427484f1dcc8ca1fdf40d7355d4a786d076
Arjmand <arjmand@Arjmands-MacBook-Air.local>
2023-04-05 23:24:49 +0330

    update app

--- fronted/karinja/src/App.js
+++ fronted/karinja/src/App.js
@@ -2,16 +2,19 @@ import React from "react";
 
 import { BrowserRouter as Router, Routes, Route, Link } from "react-router-dom";
 import DetailApplly from "./component/detailapply";
+import DetailJobCompany from "./component/detailJobCompany";
 
 import PaginatedList from "./component/Home";
 
 import Job from "./component/job";
+import JobOfCompany from "./component/jobOfCompany";
 import ListApply from "./component/listAplly";
 import Login from "./component/Login";
 import Navbar from "./component/Navbar";
 
 import SideBar from "./component/sidebar";
 import Sighnin from "./component/sighnin";
+import { useCurrent } from "./hook/current";
 
 export const App = () => {
   const access = localStorage.getItem("access");
@@ -36,16 +39,18 @@ export const App = () => {
 
               <Routes>
                 <Route path="/" element={<PaginatedList />} />
+                <Route path="/company" element={<JobOfCompany />} />
+                <Route
+                  path="/company/:pageNumber"
+                  element={<DetailJobCompany />}
+                />
                 <Route path="/:pageNumber" element={<Job />} />
                 <Route
                   path="/requests/:pageNumber"
                   element={<DetailApplly />}
                 />
-
                 <Route path="/requests" element={<ListApply />} />
-
                 <Route path="/login" element={<Login />} />
-
                 <Route path="/sighnin" element={<Sighnin />} />
               </Routes>
             </div>
